b5a830c9c1a154966fdd854d8fe144f9fad05c17
LuizGyro <luizfelipe.g@hotmail.com>
2019-05-13 18:29:06 -0300

    Begin making basic textbox

---
+++ ui/Textbox.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready():
+	pass # Replace with function body.
+	
+# Make any customizations here: load portrait, change font, change
+# textbox color, etc.
+func prepare():
+	pass
+	
+# Show text on screen. Recieves single string.
+func show_text():
+	pass
+	
+# Same as before, but recieves array of strings
+func show_multiple_text():
+	pass
+	
+	
