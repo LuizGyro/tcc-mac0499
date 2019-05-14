84c3a0f1a14300aa4334b162b5bb3c7e84fc2c58
LuizGyro <luizfelipe.g@hotmail.com>
2019-05-14 15:00:08 -0300

    Make basic text show function

--- ui/Textbox.gd
+++ ui/Textbox.gd
@@ -1,19 +1,50 @@
 extends Control
 
+const sample_text = ["Oi, meu nome é Haxixe. Você quer ser meu amiguinho? Eu adoro comer pastel. E jogar Smash. Venha ser meu amiguinho!", "É isso ai galera, chegamos na terça-feira."]
+
+var player_action = false
+
 func _ready():
-	pass # Replace with function body.
-	
+	set_process_input(true)
+
+func _input(event):
+	if (event.is_action("touch")):
+		player_action = true
+
 # Make any customizations here: load portrait, change font, change
 # textbox color, etc.
+# O que vai precisar provavelmente: se tem animação de entrada/saida da caixa,
+# o portrait, o nome do personagem, o texto, se a cena sera invertida (horizontal)
 func prepare():
 	pass
 	
 # Show text on screen. Recieves single string.
-func show_text():
-	pass
+func show_text(string):
+	var text_length = string.length()
+	for c in text_length:
+		$Text.set_text(string.substr(0, c))
+		yield($TextTimer, "timeout")
+		if player_action:
+			break
+	
+	if player_action:
+		$Text.set_text(string)
+		yield($TextTimer, "timeout")
+	
+	player_action = false
+	$CanvasLayer/Arrow/AnimationPlayer.play("Blink")
+	while(!player_action):
+		pass
+	$CanvasLayer/Arrow.hide()
+	
+	# Possibly play fadeout animation here
+	
+	queue_free()
+	
+	
 	
 # Same as before, but recieves array of strings
-func show_multiple_text():
+func show_multiple_text(s_arr):
 	pass
 	
 	
