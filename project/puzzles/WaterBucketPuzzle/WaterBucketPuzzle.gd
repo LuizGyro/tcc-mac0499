a62cfa41b3dc41ad57c289e70b0b966eaab0a0dd
LuizGyro <luizfelipe.g@hotmail.com>
2019-10-21 15:14:48 -0300

    Made sucessful transition to mushi puzzle

--- project/puzzles/WaterBucketPuzzle/WaterBucketPuzzle.gd
+++ project/puzzles/WaterBucketPuzzle/WaterBucketPuzzle.gd
@@ -3,6 +3,12 @@ extends Control
 # Normalmente, o padrão é false
 var puzzle_solved = false
 
+signal puzzle_solved
+
+func _ready():
+	GlobalFade.fade_in()
+	yield(GlobalFade.tween, "tween_completed")
+
 # Essa função se liga ao SubmitButton, para que transicionemos
 # para a tela em que a solução é entregue. Esta pode ser filha
 # desta cena mesmo, por conveniencia.
@@ -11,6 +17,9 @@ func submit_answer():
 	# Faz a animação da solução correspodente (correto ou errado)
 	if (puzzle_solved):
 		Global.correct_answer = true
+		GlobalFade.fade_out()
+		yield(GlobalFade.tween, "tween_completed")
+		emit_signal("puzzle_solved")
 		Global.transition_to_overworld()
 
 # This function checks the requirements for sucess in the puzzle,
