94633822f7e292dc5143e4fa8bb54d7d97388c23
LuizGyro <luizfelipe.g@hotmail.com>
2019-10-21 21:57:03 -0300

    Begin save

--- project/autoloads/flags.gd
+++ project/autoloads/flags.gd
@@ -13,3 +13,10 @@ var fph_first_cutscene = true
 
 # ForestPuzzleBottom
 var wb_puzzle = false
+
+func _ready():
+	var relevant_entries = []
+	for entry in get_property_list():
+		if entry.name.find("cutscene") != -1 or entry.name.find("puzzle") != -1:
+			relevant_entries.append(entry.name)
+	print(relevant_entries)
