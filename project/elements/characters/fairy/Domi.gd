7f3b4cd53c93a8f6834135ebef44c1c2ae51a82f
LuizGyro <luizfelipe.g@hotmail.com>
2019-11-02 12:49:19 -0300

    Changed Domi
    Due tu layering issues involving the Button class, we cannot have two ChoiceBoxes on the same scene. This can be fixed by changing the buttons to TouchScreen button, or some other class

--- project/elements/characters/fairy/Domi.gd
+++ project/elements/characters/fairy/Domi.gd
@@ -22,36 +22,35 @@ func interact(player):
 	player.disable_movement()
 	$Textbox.prepare_and_emit_text("Domi", ["Ei, ei, ei!! Calma ai campeão. Da para ver na sua cara que você é novo por aqui.", "Para este lado fica a saida da floresta, e as coisas não são tão receptivas quanto aqui. Só posso dar passagem para as pessoas que eu acredito que estão qualificadas.", "Podemos jogar um jogo para ver se você possui o potencial para passar. O que acha?"])
 	yield($Textbox, "textbox_done")
-	$ChoiceBox.prepare_and_emit_options("Aceitar", "Recusar")
-	var choice = yield($ChoiceBox, "choicebox_done")
-	if (choice == 1):
-		GlobalFade.fade_out()
-		player.big_zoom()
-		yield(GlobalFade.tween, "tween_completed")
-		yield(player.tween, "tween_completed")
-		Global.transition_to_puzzle(get_parent(), "CoinPuzzle")
-		yield(Global.puzzle_scene, "puzzle_solved")
-		player.reset_camera()
-		GlobalFade.fade_in()
-		yield(GlobalFade.tween, "tween_completed")
-		
-		if Global.correct_answer:
-			$Textbox.prepare_and_emit_text("Domi", ["Boa, campeão! Assim que eu gosto de ver!"], "slide_in", "pop_in")
+	$ThoughtBox.prepare_and_emit_text(Global.player_name, ["Acho que vale a pena ao menos tentar..."])
+	yield($ThoughtBox, "textbox_done")
+	$Textbox.prepare_and_emit_text("Domi", ["Me parece que você concorda! Vamos lá então."])
+	yield($Textbox, "textbox_done")
+	GlobalFade.fade_out()
+	player.big_zoom()
+	yield(GlobalFade.tween, "tween_completed")
+	yield(player.tween, "tween_completed")
+	Global.transition_to_puzzle(get_parent(), "CoinPuzzle")
+	yield(Global.puzzle_scene, "puzzle_solved")
+	player.reset_camera()
+	GlobalFade.fade_in()
+	yield(GlobalFade.tween, "tween_completed")
+	
+	if Global.correct_answer:
+		$Textbox.prepare_and_emit_text("Domi", ["Boa, campeão! Assim que eu gosto de ver!"], "slide_in", "pop_in")
+		yield($Textbox, "textbox_done")
+		PxpBar.gain_exp(20)
+		yield(PxpBar, "exp_done")
+		# Check plv to determine dialog (if demo ended, or not yet)
+		if (Global.plv >= 1):
+			$Textbox.prepare_and_emit_text("Domi", ["Estou convencido! Você pode passar... é o que eu diria, mas infelizmente não tem nada depois daqui. A você que jogou até aqui, muito obrigado por seu tempo! Pode salvar seu progresso, e ficar a vontade. Obrigado, novamente!"], "pop_in", "slide_out")
 			yield($Textbox, "textbox_done")
-			PxpBar.gain_exp(20)
-			yield(PxpBar, "exp_done")
-			# Check plv to determine dialog (if demo ended, or not yet)
-			if (Global.plv >= 1):
-				$Textbox.prepare_and_emit_text("Domi", ["Estou convencido! Você pode passar... é o que eu diria, mas infelizmente não tem nada depois daqui. A você que jogou até aqui, muito obrigado por seu tempo! Pode salvar seu progresso, e ficar a vontade. Obrigado, novamente!"], "pop_in", "slide_out")
-				yield($Textbox, "textbox_done")
-			else:
-				$Textbox.prepare_and_emit_text("Domi", ["Mas... ainda não estou convencido. Converse um pouco mais com as pessoas, se familiarize com o ambiente mais. Depois, nós conversamos novamente."], "pop_in", "slide_out")
-				yield($Textbox, "textbox_done")
-			flags.br_puzzle = true
 		else:
-			$Textbox.prepare_and_emit_text("Domi", ["É campeão, vai ter que usar a cabecinha para ganhar nesse jogo! Fica a dica: tem uma estratégia que sempre funciona para ganhar."])
+			$Textbox.prepare_and_emit_text("Domi", ["Mas... ainda não estou convencido. Converse um pouco mais com as pessoas, se familiarize com o ambiente mais. Depois, nós conversamos novamente."], "pop_in", "slide_out")
 			yield($Textbox, "textbox_done")
-		
-		player.enable_movement()
-	elif (choice == 2):
-		player.enable_movement()
+		flags.br_puzzle = true
+	else:
+		$Textbox.prepare_and_emit_text("Domi", ["É campeão, vai ter que usar a cabecinha para ganhar nesse jogo! Fica a dica: tem uma estratégia que sempre funciona para ganhar."])
+		yield($Textbox, "textbox_done")
+	
+	player.enable_movement()
